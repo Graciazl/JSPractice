d1bb3d03d083749c0bd2461649c3eb367e0b4420
Gracia <gracia@devlevelup.com>
2016-09-01 14:06:35 -0400

    no message

---
+++ script3.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Gracia on 16/8/31.
+ */
+
+(function(){
+
+    function getData() {
+        var data = [];
+
+        return data;
+    }
+
+    function sortData() {
+
+    }
+
+    function renderData() {
+
+    }
+
+    document.getElementById('sortBtn').addEventListener('click', function(){
+
+    });
+
+})();
