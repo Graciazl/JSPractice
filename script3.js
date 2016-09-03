7e54137e654ea8f66e27e64e78756d2c4364155f
Gracia <gracia@devlevelup.com>
2016-09-02 22:43:17 -0400

    Get air condition data from page.

--- script3.js
+++ script3.js
@@ -2,10 +2,24 @@
  * Created by Gracia on 16/8/31.
  */
 
+var $ = function(id) {
+    return document.getElementById(id.substr(1));
+};
+
 (function(){
 
     function getData() {
-        var data = [];
+        var data = [],
+            ul = $('#source').childElementCount;
+
+        for (i = 0; i < ul-1; i++) {
+            var child = $('#source').children[i],
+                str = child.innerHTML.split(':'),
+                air = str[1],
+                str1 = str[0].split('of'),
+                city = str1[1];
+            data.push(city,air);
+        }
 
         return data;
     }
