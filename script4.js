bff8b7dac71190a3f2371973eb0890d1f1adf61b
Gracia <gracia@devlevelup.com>
2016-09-08 22:35:18 -0400

    Add event listener to validate the input.

--- script4.js
+++ script4.js
@@ -3,16 +3,26 @@
  */
 
 
-var $ = function(id) {
+var $ = function (id) {
     return document.getElementById(id.substr(1));
 };
 
-(function(){
+(function () {
     var data = [],
+        city = $('#cityInput'),
+        air = $('#airInput'),
         list = {
-            city : $('#cityInput').value,
-            air : $('#airInput').value
+            cityName: city.value,
+            airIndex: air.value
         };
+
+    function addData() {
+
+    }
+
+    function isInteger(x) {
+        return x % 1 === 0;
+    }   // Check if the number is an integer
 
     function createNewRow() {
         var newCell = document.createElement('td'),
@@ -23,7 +33,41 @@ var $ = function(id) {
         newRow.appendChild(newRow);
         $('#tableData').appendChild(newRow);
     }
-    
+
+    function delBtn() {
+
+    }
+
+    city.addEventListener('blur', function() {
+        var p1 = $('#alertP1');
+
+        if (city.validity.valueMissing) {
+            p1.innerHTML = 'City name should not be empty.';
+        } else if (city.validity.patternMismatch) {
+            p1.innerHTML = 'Please enter right name of city.';
+        } else {
+            p1.innerHTML = '';
+        }
+    });
+
+    air.addEventListener('blur', function() {
+        var p2 = $('#alertP2');
+
+        if (air.validity.valueMissing) {
+            p2.innerHTML = 'Air index should not be empty.';
+        } else if (isNaN(air.value)) {
+            p2.innerHTML = 'Please enter the valid value of air index.';
+        } else if (isInteger(air.value) === false ) {
+            p2.innerHTML = 'Valid value of air index should be integer.';
+        } else {
+            p2.innerHTML = '';
+        }
+    });
+
+    $('#add-btn').addEventListener('click', function () {
+
+    });
+
 
 })();
 
