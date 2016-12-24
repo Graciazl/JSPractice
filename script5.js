50ef63c2bf92d508554c1fce5d5cde36e2032e3a
Gracia <gracia@devlevelup.com>
2016-12-23 19:17:58 -0500

    Generate date automatically.

--- script5.js
+++ script5.js
@@ -1,3 +1,35 @@
 /**
  * Created by Gracia on 16/12/17.
  */
+
+function randomBuildData(seed) {
+    var returnData = {},
+        dat = new Date("2016-01-01"),
+        datStr = '';
+
+    for (var i = 1; i < 92; i++) {
+        datStr = getDateStr(dat);
+        returnData[datStr] = Math.ceil(Math.random() * seed);
+        dat.setDate(dat.getDate() + 1);
+    }
+
+    return returnData;
+}
+
+function getDateStr(dat) {
+    var y = dat.getFullYear(),
+        m = dat.getMonth() + 1,
+        d = dat.getDate();
+
+    m = m < 10 ? '0' + m : m;
+    d = d < 10 ? '0' + d : d;
+
+    return y + '-' + m + '-' + d;
+}
+
+var aqiSourceData = {
+    "Beijing": randomBuildData(500),
+    "Singapore": randomBuildData(100),
+    "Mexico": randomBuildData(500),
+    "Toronto": randomBuildData(80)
+};
