3471b8aadafe91767a374da33c6169359e57b1f2
Gracia <gracia@devlevelup.com>
2017-01-06 20:51:54 -0500

    no message

--- script8.js
+++ script8.js
@@ -1,3 +1,37 @@
 /**
  * Created by Gracia on 17/1/4.
  */
+var arr = [],
+    input = document.getElementById('textInput'),
+    submit = document.getElementById('submit'),
+    result = document.getElementById('result');
+
+function getInput() {
+    var text = input.value;
+    return text.split(' ');
+}
+
+function render(arr) {
+    result.innerHTML = '';
+
+    if (arr.length !== 0) {
+        for (var i = 0; i < arr.length; i++) {
+            var newDiv = document.createElement('div'),
+                text = document.createTextNode(arr[i]);
+
+            newDiv.style.background = 'red';
+            newDiv.style.color = 'white';
+            newDiv.style.padding = '10px';
+            newDiv.style.margin = '10px';
+            newDiv.style.display = 'inline-block';
+
+            newDiv.appendChild(text);
+            result.appendChild(newDiv);
+        }
+    }
+}
+
+submit.addEventListener('click', function() {
+    arr = getInput();
+    render(arr);
+});
